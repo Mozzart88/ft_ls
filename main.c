83f1c305c22cbd423951a80c39d457eba5369bf7
Mozzart <forspamorelse@gmail.com>
2020-01-12 22:27:52 +0300

    [IN PROGRESS]

--- main.c
+++ main.c
@@ -6,15 +6,34 @@
 /*   By: tvanessa <tvanessa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/11 22:30:02 by tvanessa          #+#    #+#             */
-/*   Updated: 2020/01/11 22:31:17 by tvanessa         ###   ########.fr       */
+/*   Updated: 2020/01/12 20:48:47 by tvanessa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int		main(int ac, char *av)
+
+int		main(int ac, char **av)
 {
-	if (ac || av)
-		return (0);
+	char	*dname;
+	t_us	i;
+	DIR		*d;
+	t_de	*dc;
+
+	i = 0;
+	while (i < ac)
+	{
+		if (ac == 1 && ++i)
+			dname = ".";
+		else
+			dname = av[++i];
+		if ((d = opendir(dname)))
+			while ((dc = readdir(d)))
+			{
+				ft_printf("%s -> ", dc->d_name);
+				dc->d_type == 8 ? ft_printf("file\n") : ft_printf("dir\n");
+			}
+		closedir(d);
+	}	
 	return (0);
 }
