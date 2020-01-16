0f7f2a8efb2e4bfab98849a18b7481646d666de8
Mozzart <forspamorelse@gmail.com>
2020-01-16 23:33:55 +0300

    [IN PROGRESS]
    -lGRfa flags  realization

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tvanessa <tvanessa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/11 22:30:02 by tvanessa          #+#    #+#             */
-/*   Updated: 2020/01/16 15:23:40 by tvanessa         ###   ########.fr       */
+/*   Updated: 2020/01/16 23:32:39 by tvanessa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,24 @@
 #define WORD_FLAGS {}
 #define STRICT_FLAGS_ORDER 1
 typedef struct stat	t_stat;
+typedef struct timespec	t_time;
+typedef struct passwd	t_pwd;
+typedef struct group	t_grp;
+typedef	struct		s_rec
+{
+	t_de			*de;
+	t_stat			*st;
+	struct s_rec	*conten[512];
+	void			(*sort)(struct s_rec*, char[10]);
+	void			(*print)(struct s_rec*, char[10]);
+	char			path[__DARWIN_MAXPATHLEN];
+}					t_rec;
+
 
 void	ft_get_flags(t_us *arr, char **av, int *len)
 {
 	char	*ai;
 
-	// ai = -1;
 	while (--*len)
 	{
 		if (*av[0] != '-' && STRICT_FLAGS_ORDER)
@@ -40,7 +52,6 @@ void	ft_get_flags(t_us *arr, char **av, int *len)
 		}
 		++av;
 	}
-	// arr[++ai] = 0;
 }
 
 void	ft_get_params(char **arr, char **av, int len)
@@ -59,18 +70,40 @@ void	ft_get_params(char **arr, char **av, int len)
 	arr[i] = NULL;
 }
 
-t_de	*ft_copydc(t_de *dc)
+t_de	*ft_copyde(t_de *de)
 {
 	t_de	*r;
 
 	if (!(r = (t_de*)malloc(sizeof(t_de))))
 		return (NULL);
-	r->d_ino = dc->d_ino;
-	ft_strcpy(r->d_name, dc->d_name);
-	r->d_namlen = dc->d_namlen;
-	r->d_reclen = dc->d_reclen;
-	r->d_seekoff = dc->d_seekoff;
-	r->d_type = dc->d_type;
+	r->d_ino = de->d_ino;
+	ft_strcpy(r->d_name, de->d_name);
+	r->d_namlen = de->d_namlen;
+	r->d_reclen = de->d_reclen;
+	r->d_seekoff = de->d_seekoff;
+	r->d_type = de->d_type;
+	return (r);
+}
+
+t_rec	*ft_new_rec(t_de *de, char *name, char path[__DARWIN_MAXPATHLEN])
+{
+	t_rec	*r;
+
+	r = NULL;
+	if (!(r = (t_rec*)malloc(sizeof(t_rec))))
+		return (NULL);
+	ft_memset(r->path, 0, __DARWIN_MAXPATHLEN);
+	ft_strcat(r->path, path);
+	ft_strcat(r->path, name);
+	if (de)
+	{
+		r->de = ft_copyde(de);
+	}
+	if (!(r->st = (t_stat*)malloc(sizeof(t_stat))))
+		return (NULL);
+	name = r->path[0] ? r->path : name;
+	if (stat(name, r->st))
+		return (NULL);
 	return (r);
 }
 
@@ -94,76 +127,166 @@ char	ft_get_file_type(mode_t m)
 	return ('-');
 }
 
-void	ft_print_stat(t_de *de)
+void	ft_print_stat(mode_t mode)
 {
-	t_stat	*st;
+	// t_stat	*st;
 	int		i;
 	int		j;
 	char	m[11];
 	char	*t;
 
 	t = "rwxrwxrwx";
-	if (!(st = (t_stat*)malloc(sizeof(t_stat))))
-		return ;
-	stat(de->d_name, st);
-	m[0] = ft_get_file_type(st->st_mode);
+	m[0] = ft_get_file_type(mode);
 	i = 1;
 	j = 1 << 8;
 	while (i < 10)
 	{
-		m[i] = st->st_mode & j ? t[i - 1] : '-';
+		m[i] = mode & j ? t[i - 1] : '-';
 		++i;
 		j >>= 1;
 	}
 	m[10] = '\0';
-	ft_printf("%s\t", m);
+	ft_printf("%s", m);
 }
 
-void	ft_printdir(char *dname, t_us *flags)
+void	ft_print_user(uid_t id)
+{
+	t_pwd *p;
+	if (!id)
+		return ;
+	if (!(p = getpwuid(id)))
+		return ;
+	ft_printf("%9s", p->pw_name);
+	// free(p);
+}
+
+void	ft_print_group(gid_t id)
+{
+	t_grp *g;
+	if (!id)
+		return ;
+	if (!(g = getgrgid(id)))
+		return ;
+	ft_printf("%6s", g->gr_name);
+	// free(g);
+}
+
+void	ft_print_time(t_time t)
+{
+	char	*ct;
+	char	**rt;
+
+	ct = ctime(&t.tv_sec);
+	rt = ft_strsplit(ct, ' ');
+	// if (t.tv_sec)
+	ft_printf(" %s %s %5s", rt[1], rt[2], rt[3]);
+}
+
+void	ft_print_filename(t_rec *r, t_us color)
+{
+	if (!color)
+		ft_printf(" %s\n", r->de->d_name);
+	else
+	{
+		if (r->de->d_type == 4)
+			ft_printf(" \e[34m%s\e[0m\n", r->de->d_name);
+		else if (r->st->st_mode & 0b1001001)
+			ft_printf(" \e[31m%s\e[0m\n", r->de->d_name);
+		else
+			ft_printf(" %s\n", r->de->d_name);
+	}
+}
+
+void	ft_printdir(t_rec **rd, t_us *f)
+{
+	t_us	i;
+
+	i = 0;
+	if (!*rd)
+	 return ;
+	if (f[5])
+	{
+		ft_print_stat((*rd)->st->st_mode);
+		ft_printf("%5 hu", (*rd)->st->st_nlink);
+		if (!f[3])
+			ft_print_user((*rd)->st->st_uid);
+		ft_print_group((*rd)->st->st_gid);
+		ft_printf("%7 lld", (*rd)->st->st_size);
+		if (f[9])
+			ft_print_time((*rd)->st->st_atimespec);
+		else
+			ft_print_time((*rd)->st->st_mtimespec);
+	}
+	ft_print_filename((*rd), f[4]);
+	if (*(rd + 1))
+		ft_printdir(rd + 1, f);
+}
+
+void	ft_dir_sort(t_rec **d, t_us *f)
+{
+	if (d && f)
+		return ;
+}
+
+void	ft_readdir(char *dname, t_us *flags)
 {
 	DIR		*d;
 	t_de	*dc;
-	t_de	*rd[512];
+	t_rec	*rd[512];
 	t_us	i;
-	t_us	di;
-	char	*dn;
+	char	path[__DARWIN_MAXPATHLEN];
 
 	i = 0;
-	if ((d = opendir(dname)))
+	ft_memset(path, 0, __DARWIN_MAXPATHLEN);
+	ft_strcpy(path, dname);
+	ft_strcat(path, "/");
+	if (!flags[1] && (d = opendir(dname)))
 	{
 		while ((dc = readdir(d)))
 		{
-			if (!flags[0] && dc->d_name[0] == '.')
+			if ((!flags[0] && !flags[2]) && dc->d_name[0] == '.')
 				continue;
-			if (dc->d_type == 4 && flags[7] && !((dc->d_namlen == 1 && dc->d_name[0] == '.') || (dc->d_namlen == 2 && !ft_strcmp(dc->d_name, ".."))))
-			{
-				rd[i] = ft_copydc(dc);
-				++i;
-			}
-			if (flags[5])
-				ft_print_stat(dc);				
-			ft_printf("%s -> %d\n", dc->d_name, dc->d_type);
+			rd[i] = ft_new_rec(dc, dc->d_name, path);
+			++i;
 		}
 		closedir(d);
 	}
-	// else if (st->)
+	else if (!(rd[i] = ft_new_rec(NULL, dname, path)))
+		return ;
 	else
 		ft_printf("ft_ls: %s: No such file or directory\n", dname);
-	di = 0;
-	dn = NULL;
-	while (di < i)
-	{
-		ft_printf("\n%s/%s:\n", dname, (rd[di])->d_name);
-		dn = ft_strnew(ft_strlen(dname) + ft_strlen((rd[di])->d_name) + 1);
-		ft_strcpy(dn, dname);
-		dn[ft_strlen(dname)] = '/';
-		ft_strcpy(dn + ft_strlen(dname) + 1, (rd[di])->d_name);
-		ft_printdir(dn, flags);
-		ft_strdel(&dn);
-		free(rd[di]);
-		rd[di] = NULL;
-		++di;
-	}
+	rd[i] = NULL;
+	if (!flags[2])
+		ft_dir_sort(rd, flags);
+	ft_printdir(rd, flags);
+	i = 0;
+	if (flags[7])
+		while (rd[i])
+		{
+			if (rd[i]->de->d_type == 4 && !((rd[i]->de->d_name[0] == '.' && rd[i]->de->d_namlen == 1) || (ft_strequ(rd[i]->de->d_name, "..") && rd[i]->de->d_namlen == 2)))
+			{
+				ft_printf("\n%s:\n", rd[i]->path);
+				ft_readdir((rd[i]->path), flags);
+				// ft_printf("\n");
+			}
+			++i;
+		}
+
+	// di = 0;
+	// dn = NULL;
+	// while (rd[di])
+	// {
+	// 	ft_printf("\n%s/%s:\n", dname, (rd[di])->d_name);
+	// 	dn = ft_strnew(ft_strlen(dname) + ft_strlen((rd[di])->d_name) + 1);
+	// 	ft_strcpy(dn, dname);
+	// 	dn[ft_strlen(dname)] = '/';
+	// 	ft_strcpy(dn + ft_strlen(dname) + 1, (rd[di])->d_name);
+	// 	ft_printdir(dn, flags);
+	// 	ft_strdel(&dn);
+	// 	free(rd[di]);
+	// 	rd[di] = NULL;
+	// 	++di;
+	// }
 }
 
 int		main(int ac, char **av)
@@ -184,12 +307,12 @@ int		main(int ac, char **av)
 	ft_get_params(params, (av + (i - ac - 1)), ac);
 	i = 0;
 	if (!params[i])
-		ft_printdir(".", flags);
+		ft_readdir(".", flags);
 	while (params[i])
 	{
 		if (i > 0 || params[i + 1])
 			ft_printf("%s:\n", params[i]);
-		ft_printdir(params[i], flags);
+		ft_readdir(params[i], flags);
 		if (params[i + 1])
 			ft_printf("\n");
 		++i;
