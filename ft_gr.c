bbd4f8de90135865a6e23ee8b3b02f5ed897c1f6
Ivan Timokhov <timokhovim@gmail.com>
2020-05-18 22:23:50 +0300

    [IN PROGRESS]
    - forbidden functions

--- ft_gr.c
+++ ft_gr.c
@@ -6,7 +6,7 @@
 /*   By: mozzart <mozzart@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/17 10:16:57 by mozzart           #+#    #+#             */
-/*   Updated: 2020/05/18 20:56:41 by mozzart          ###   ########.fr       */
+/*   Updated: 2020/05/18 22:20:58 by mozzart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,10 @@ char	*ft_get_group_name(gid_t id)
 {
 	t_grp	*g;
 	char	*name;
-	char	buff[_SC_GETGR_R_SIZE_MAX * MAXLOGNAME];
 
-	if (!(g = (t_grp*)malloc(sizeof(t_grp))))
-		return (NULL);
-	if ((getgrgid_r(id, g, buff, _SC_GETGR_R_SIZE_MAX * MAXLOGNAME, &g)) || !g)
+	if (!(g = getgrgid(id)))
 		name = ft_itoa_long_un(id);
 	else
 		name = ft_strdup(g->gr_name);
-	g->gr_mem = NULL;
-	g->gr_passwd = NULL;
-	g->gr_name = NULL;
-	g->gr_gid = 0;
-	free(g);
-	g = NULL;
 	return (name);
 }
--- ft_pwd.c
+++ ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: mozzart <mozzart@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/17 10:07:32 by mozzart           #+#    #+#             */
-/*   Updated: 2020/05/18 20:55:32 by mozzart          ###   ########.fr       */
+/*   Updated: 2020/05/18 22:19:50 by mozzart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,37 +26,14 @@ void		ft_print_user(uid_t id, t_ull un, uint32_t f)
 	}
 }
 
-static void	ft_free_pwd(t_pwd **p)
-{
-	if (p && *p)
-	{
-		(*p)->pw_name = NULL;
-		(*p)->pw_passwd = NULL;
-		(*p)->pw_class = NULL;
-		(*p)->pw_gecos = NULL;
-		(*p)->pw_dir = NULL;
-		(*p)->pw_shell = NULL;
-		(*p)->pw_change = 0;
-		(*p)->pw_expire = 0;
-		(*p)->pw_uid = 0;
-		(*p)->pw_gid = 0;
-		free(*p);
-		*p = NULL;
-	}
-}
-
 char		*ft_get_user_name(uid_t id)
 {
 	t_pwd	*p;
 	char	*name;
-	char	buff[_SC_GETPW_R_SIZE_MAX];
 
-	if (!(p = (t_pwd*)malloc(sizeof(t_pwd))))
-		return (NULL);
-	if ((getpwuid_r(id, p, buff, _SC_GETPW_R_SIZE_MAX, &p)) || !p)
+	if (!(p = getpwuid(id)))
 		name = ft_itoa_long_un(id);
 	else
 		name = ft_strdup(p->pw_name);
-	ft_free_pwd(&p);
 	return (name);
 }
