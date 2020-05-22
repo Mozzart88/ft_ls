5ff9c9f15ec43bc226906f8e2e61b3dbe4803c7e
Ivan Timokhov <timokhovim@gmail.com>
2020-05-22 04:31:48 +0300

    [NORME]
    - vector.c

--- vector.c
+++ vector.c
@@ -6,13 +6,13 @@
 /*   By: mozzart <mozzart@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/09 04:28:02 by mozzart           #+#    #+#             */
-/*   Updated: 2020/05/22 04:13:23 by mozzart          ###   ########.fr       */
+/*   Updated: 2020/05/22 04:31:21 by mozzart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-t_vect	*ft_new_vect(/* size_t s,  */size_t l, void *f(void**))
+t_vect	*ft_new_vect(size_t l, void *f(void**))
 {
 	t_vect	*v;
 
@@ -23,7 +23,6 @@ t_vect	*ft_new_vect(/* size_t s,  */size_t l, void *f(void**))
 		v = NULL;
 		free(v);
 	}
-	// v->size = s;
 	v->len = l;
 	v->ilen = l;
 	v->is_sorted = 0;
@@ -38,12 +37,10 @@ void	*ft_destroy_vect(t_vect **v)
 	while ((*v)->len-- > 0)
 	{
 		if ((p = (void*)&((*v)->arr[(*v)->len])))
-		// p = (void*)&((*v)->arr[(*v)->len]);
 			if ((*v)->arr_destroier)
 				(*v)->arr_destroier(p);
 		(*v)->arr[(*v)->len] = NULL;
 	}
-	// (*v)->size = 0;
 	(*v)->len = 0;
 	(*v)->ilen = 0;
 	(*v)->is_sorted = 0;
