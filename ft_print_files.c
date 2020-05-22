10b66673f05c5318d12208db7c9fb05b81d00066
Ivan Timokhov <timokhovim@gmail.com>
2020-05-22 06:15:57 +0300

    [NORME]
    - ft_print_files.c

--- ft_print_files.c
+++ ft_print_files.c
@@ -6,7 +6,7 @@
 /*   By: mozzart <mozzart@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/17 12:13:17 by mozzart           #+#    #+#             */
-/*   Updated: 2020/05/20 20:27:18 by mozzart          ###   ########.fr       */
+/*   Updated: 2020/05/22 06:15:39 by mozzart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,11 @@ void	ft_print_files(t_vect *r, uint32_t *f, t_maxvallen mvl)
 	{
 		rec = ((t_rec*)(r->arr[i]));
 		p = ((t_rec*)(r->arr[i]))->name[0] == '.' ? 1 : 0;
-		// if (*f & FT_ZP_BIT)
-		// 	p = 0;
 		if (!p || *f & AE_FLAGS)
 		{
 			ft = ft_get_file_type(rec->st->st_mode);
 			if (ft != 'd' && (ft != 'l' || *f & LF_FLAGS))
 				ft_print_rifo((t_rec*)r->arr[i], f, mvl);
-			// else if (ft == 'l' && !(ft_is_dir(rec->lnk_to->st->st_mode)))
 			else if (ft == 'l' && !(ft_is_dir(rec->lnk_to)))
 				ft_print_rifo((t_rec*)r->arr[i], f, mvl);
 		}
