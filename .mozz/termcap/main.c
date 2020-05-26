910f4382090982c769694a3f9c328c13fb80f38c
Ivan Timokhov <timokhovim@gmail.com>
2020-05-26 17:43:44 +0300

    [IN PROGRESS]
    - in terminal looks like done, but not in file

--- .mozz/termcap/main.c
+++ .mozz/termcap/main.c
@@ -6,7 +6,7 @@
 /*   By: mozzart <mozzart@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/25 11:22:59 by mozzart           #+#    #+#             */
-/*   Updated: 2020/05/26 16:58:59 by mozzart          ###   ########.fr       */
+/*   Updated: 2020/05/26 17:38:49 by mozzart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,11 +45,11 @@ int main(void)
 	tgetent(b, getenv("TERM"));
 
 	tparm_value = tparm(save_cursor);
-	putp(tparm_value);
+		tputs(tparm_value, 1, ftt_putchar);
 	tparm_value = tparm(cursor_visible);
 	putp(tparm_value);
 
-	str_max_len = ft_strlen(string);
+	str_max_len = ft_strlen(string) + 2;
 	columns_count = columns;
 	margin = str_max_len % TABSIZE ? TABSIZE - str_max_len % TABSIZE + str_max_len : str_max_len + TABSIZE;
 
@@ -59,19 +59,48 @@ int main(void)
 	rows_count = d / (columns_count / margin) ? d / (columns_count / margin) : 1;
 	++rows_count;
 	i = 1;
-	r = 1;
+	r = 0;
 	c = 0;
+	ft_printf("%d", lines_of_memory);
 	while (i < d)
 	{
-		tparm_value = tparm(cursor_address, r, c);
+		// tparm_value = tparm(cursor_address, r, c);
+		tparm_value = tparm(column_address, c);
 		tputs(tparm_value, 1, ftt_putchar);
-		sleep(1);
-		ft_printf("%s", string);
-		r = i == 0 || i % rows_count ? r + 1 : 1;
-		c = i == 0 ? 0 : i / rows_count * margin;
+		// sleep(1);
+		ft_printf("%s%02d", string, i);
+		r = i % rows_count ? r + 1 : 0;
+		c = i / rows_count * margin;
+		if (r)
+		{
+			tparm_value = tparm(cursor_down);
+			tputs(tparm_value, 1, ftt_putchar);
+			// sleep(1);
+		}
+		else
+		{
+			tparm_value = tparm(restore_cursor);
+			tputs(tparm_value, 1, ftt_putchar);
+			// sleep(1);
+		}
+		
 		++i;
+		if (i >= d)
+		{
+			// sleep(1);
+			while (r < rows_count)
+			{
+				tparm_value = tparm(cursor_down);
+				tputs(tparm_value, 1, ftt_putchar);
+				++r;
+				sleep(1);
+			}
+
+			// tparm_value = tparm(column_address, c);
+			// tputs(tparm_value, 1, ftt_putchar);
+		}
 	}
-	tparm_value = tparm(cursor_address, rows_count, c);
-	tputs(tparm_value, 1, ftt_putchar);
+	// tparm_value = tparm(cursor_address, rows_count, c);
+	// tputs(tparm_value, 1, ftt_putchar);
 	return (0);
 }
