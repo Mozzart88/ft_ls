c3f0bfb2be82f5e8753795b9d398ced1cefedf0b
Ivan Timokhov <timokhovim@gmail.com>
2020-05-31 14:06:44 +0300

    [LIBFT]

--- .mozz/startvm.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/zsh
-VirtualBoxVM --startvm "macOS" &
--- .mozz/valg.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --show-reachable=yes --leak-resolution=high --read-inline-info=yes --read-var-info=yes --show-error-list=yes $1
